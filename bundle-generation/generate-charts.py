8b177f44b387cddcb6a59627d3320d9c9aa35fde
Cameron Wall <cwall@redhat.com>
2025-02-05 10:53:19 -0500

    update release namespace
    Signed-off-by: Cameron Wall <cwall@redhat.com>

--- bundle-generation/generate-charts.py
+++ bundle-generation/generate-charts.py
@@ -650,7 +650,7 @@ def updateHelmResources(chartName, helmChart, exclusions, inclusions, branch):
                     if kind == 'ConfigMap':
                         resource_data['metadata']['namespace'] = '{{ .Values.global.namespace  }}'
                         if 'config.yaml' in resource_data['data']:
-                            logging.warning("we are attempting to make changes")
+                            logging.warning(resource_data['data']['config.yaml'])
                             # resource_data['data']['config.yaml'] = resource_data['data']['config.yaml'].replace('default', '{{ .Values.global.namespace  }}')
                             replace_default(resource_data, 'default', '{{ .Values.global.namespace  }}')
                             # resource_data['data']['config.yaml'] = resource_data['data']['config.yaml'].replace('placeholder-url', '{{ .Values.global.aPIUrl  }}')
